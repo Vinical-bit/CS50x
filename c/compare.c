f1e1eb2d3151a31d39a41b71e0684b351d8f03ac
Vinical-bit <vinical7.c.cavalcanti@gmail.com>
2026-04-29 16:47:07 -0300

    praticando

---
+++ c/compare.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+    
+    printf("what's x ? ");
+    scanf("%d", &x);
+    
+    printf("what's y ?");
+    scanf("%d", &y);
+
+    if (x < y) {
+        printf(" x is less than y\n");
+    }
+    else if (x > y) {
+        printf("x is greater than y\n");
+    }
+    
+    else{
+        printf( "x is equal to y\n");
+    }
+
+    return 0;
+}
