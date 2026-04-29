9c15ac1542ff2e59fe226d80119aa39b249edd72
Vinical-bit <vinical7.c.cavalcanti@gmail.com>
2026-04-29 14:12:19 -0300

    first step

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello, world\n"); // O /N É para quebrar uma linha do terminal e não ficar o proximo comando na mesma linha. é tipo o hr de html//
+}
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int idade;
+
+    printf("Sua idade e: ");
+    scanf("%d", &idade);
+
+    printf("Parabens pelos seus %d anos de vida!", idade);
+
+    return 0;
+}
